fe7b6c157f481e36d97b4000753d131138378b90
Ryan Nkansah <39705812+ryan2068@users.noreply.github.com>
2023-11-29 11:18:44 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,10 +1,8 @@
 const mergeSort = (unmergedArray) => {
-  const firstHalf = unmergedArray.slice(0, 2)
-  firstHalf.forEach((element, index) => {
-    console.log(element)
-  });
+  const firstHalf = unmergedArray.slice(0, (unmergedArray.length / 2))
+  secondHalf = 
 
 
 }
 
-console.log(mergeSort([1,4,5,8]))
+console.log(mergeSort([4,2,11,1,20,17,5,8]))
